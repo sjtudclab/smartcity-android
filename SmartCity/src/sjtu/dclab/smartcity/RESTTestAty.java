f8a240446f500b6e97a0bf3b4b8744de74c73446
ernestyj@outlook.com <ernestyj@outlook.com>
2015-07-10 16:55:24 +0800

    commit error fix

--- SmartCity/src/sjtu/dclab/smartcity/RESTTestAty.java
+++ SmartCity/src/sjtu/dclab/smartcity/RESTTestAty.java
@@ -2,14 +2,8 @@ package sjtu.dclab.smartcity;
 
 import android.app.Activity;
 import android.os.Bundle;
-<<<<<<< HEAD
 
 import sjtu.dclab.smartcity.tools.Constants;
-=======
-import android.view.View;
-import android.widget.Button;
-
->>>>>>> b24116a7aa04cbf811e8f374582f4101f0c4df64
 import sjtu.dclab.smartcity.tools.RESTTest;
 
 /**RESTTestAty extends Activity
@@ -19,17 +13,13 @@ import sjtu.dclab.smartcity.tools.RESTTest;
  * @date 2015/7/9
  */
 public class RESTTestAty extends Activity {
-<<<<<<< HEAD
     static final String TAG = "RESTTestAty";
 
-=======
->>>>>>> b24116a7aa04cbf811e8f374582f4101f0c4df64
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aty_main);
 
-<<<<<<< HEAD
 //        Button btn1 = (Button) findViewById(R.id.btn1);
 //        btn1.setVisibility(View.VISIBLE);
 //        btn1.setText("");
@@ -42,14 +32,6 @@ public class RESTTestAty extends Activity {
 //                Constants.URL_REST_MAILBOX_BASE, "3", Constants.DISCUSS);
 //        RESTTest.RequestForJson(getApplicationContext(),
 //                Constants.URL_REST_MAILBOX_BASE, "3", Constants.TRANSFER);
-=======
-        Button btn1 = (Button) findViewById(R.id.btn1);
-        btn1.setVisibility(View.VISIBLE);
-        btn1.setText("R");
-
-        RESTTest.RequestForJson(getApplicationContext(),
-                RESTTest.URL_REST_MAILBOX_BASE, "3", RESTTest.WAIT);
->>>>>>> b24116a7aa04cbf811e8f374582f4101f0c4df64
 
     }
 }
