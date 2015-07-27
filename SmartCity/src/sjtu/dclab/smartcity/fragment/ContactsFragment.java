0e17f6cca06b76b3a4d8d8b517e5f9ce7ef699a1
冯瀚 <fenghan0620@hotmail.com>
2015-07-27 22:20:51 +0800

    feng

--- SmartCity/src/sjtu/dclab/smartcity/fragment/ContactsFragment.java
+++ SmartCity/src/sjtu/dclab/smartcity/fragment/ContactsFragment.java
@@ -25,15 +25,27 @@ import sjtu.dclab.smartcity.utility.SerializableMap;
  * Created by hp on 2015/7/21.
  */
 public class ContactsFragment extends Fragment {
-   // private getDataListener mListener;
 
-    private TextView questText;
-    private TextView friendText;
-    private TextView groupText;
-
-    private ListView questList;
-    private ListView groupList;
-    private ListView firendList;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_contacts, container, false);
+        return view;
+    }
+//   // private getDataListener mListener;
+//
+//    private TextView questText;
+//    private TextView friendText;
+//    private TextView groupText;
+//
+//    private ListView questList;
+//    private ListView groupList;
+//    private ListView firendList;
+//
+//    public static ContactsFragment newInstance(Bundle bundle){
+//        ContactsFragment f = new ContactsFragment();
+//        f.setArguments(bundle);
+//        return f;
+//    }
 
 //    public ContactsFragment(){super();}
 //    public ContactsFragment(Bundle b){
@@ -55,35 +67,36 @@ public class ContactsFragment extends Fragment {
     }
 
 */
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
-    {
-        View view = inflater.inflate(R.layout.fragment_contacts, container, false);
-        Bundle bundle = getArguments();
-        Bundle b = getActivity().getIntent().getExtras();
-    //    System.out.println("System output bundle:" + bundle);
-    //    System.out.println("System output bundle:" + bundle.getSerializable("questMap"));
-        SerializableMap questMap = (SerializableMap) bundle.getSerializable("questMap");
-     //   System.out.println("System output bundle:" + getFragmentManager());
-     //   System.out.println("System output bundle:" + getFragmentManager().findFragmentByTag("1234567").getArguments());
-        SerializableMap friendMap = new SerializableMap();
-        SerializableMap groupMap = new SerializableMap();
-     //   SerializableMap questMap = (SerializableMap) getFragmentManager().findFragmentByTag("1234567").getArguments().getSerializable("questMap"); //new HashMap<String,Object>();
-      //  SerializableMap friendMap = (SerializableMap) getFragmentManager().findFragmentByTag("1234567").getArguments().getSerializable("friendMap");
-      //  SerializableMap groupMap = (SerializableMap) getFragmentManager().findFragmentByTag("1234567").getArguments().getSerializable("groupMap");
-
-        //List<String> questName = new ArrayList<String>();
-        //String[] questName = new String[] {""};
-
-        questList =  (ListView) view.findViewById(R.id.questList);
-    //    SimpleAdapter simpleAdapter = new SimpleAdapter(this.getActivity(), getData(questMap.getMap()),
-    //            R.layout.contacts_questlist, new String[] {"name"}, new int[] {R.id.quest_name});
-    //    questList.setAdapter(simpleAdapter);
-
-        return view;
-    }
-
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState)
+//    {
+//        View view = inflater.inflate(R.layout.fragment_contacts, container, false);
+//   /*
+//        Bundle bundle = getArguments();
+//        Bundle b = getActivity().getIntent().getExtras();
+//    //    System.out.println("System output bundle:" + bundle);
+//    //    System.out.println("System output bundle:" + bundle.getSerializable("questMap"));
+//        SerializableMap questMap = (SerializableMap) bundle.getSerializable("questMap");
+//     //   System.out.println("System output bundle:" + getFragmentManager());
+//     //   System.out.println("System output bundle:" + getFragmentManager().findFragmentByTag("1234567").getArguments());
+//        SerializableMap friendMap = new SerializableMap();
+//        SerializableMap groupMap = new SerializableMap();
+//     //   SerializableMap questMap = (SerializableMap) getFragmentManager().findFragmentByTag("1234567").getArguments().getSerializable("questMap"); //new HashMap<String,Object>();
+//      //  SerializableMap friendMap = (SerializableMap) getFragmentManager().findFragmentByTag("1234567").getArguments().getSerializable("friendMap");
+//      //  SerializableMap groupMap = (SerializableMap) getFragmentManager().findFragmentByTag("1234567").getArguments().getSerializable("groupMap");
+//
+//        //List<String> questName = new ArrayList<String>();
+//        //String[] questName = new String[] {""};
+//
+//        questList =  (ListView) view.findViewById(R.id.questList);
+//    //    SimpleAdapter simpleAdapter = new SimpleAdapter(this.getActivity(), getData(questMap.getMap()),
+//    //            R.layout.contacts_questlist, new String[] {"name"}, new int[] {R.id.quest_name});
+//    //    questList.setAdapter(simpleAdapter);
+//*/
+//        return view;
+//    }
+    /*
     private List<Map<String, Object>> getData(Map<String, Object> m) {
         List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 
@@ -97,4 +110,5 @@ public class ContactsFragment extends Fragment {
 
         return list;
     }
+   */
 }
