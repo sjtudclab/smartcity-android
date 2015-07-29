cef9c0860c1350160be056e8007b70bc0dde168e
朱宇 <zhuyu199428@sina.com>
2015-07-30 01:10:32 +0800

    update 7-29

---
+++ SmartCity/src/sjtu/dclab/smartcity/ui/party/ThouhtReportAty.java
@@ -0,0 +1,17 @@
+package sjtu.dclab.smartcity.ui.party;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import sjtu.dclab.smartcity.R;
+
+/**
+ * Created by theGODofws on 2015/7/29.
+ */
+public class ThouhtReportAty extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pty_thought_report);
+    }
+}
