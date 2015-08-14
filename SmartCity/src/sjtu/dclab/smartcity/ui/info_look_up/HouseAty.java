f4c3dd17b65dd04e470c5d7c1adca646f905ccec
朱宇 <zhuyu199428@sina.com>
2015-08-14 14:58:00 +0800

    房屋信息与人口信息初步完成并实现，有待测试以及功能完善

--- SmartCity/src/sjtu/dclab/smartcity/ui/info_look_up/HouseAty.java
+++ SmartCity/src/sjtu/dclab/smartcity/ui/info_look_up/HouseAty.java
@@ -19,58 +19,42 @@ import sjtu.dclab.smartcity.R;
  */
 public class HouseAty extends Activity {
     //spinner gender
-    private Spinner spinner_gender;
-    private List<String> list_gender;
-    private ArrayAdapter<String> adapter_gender;
-    //spinner income
-    private Spinner spinner_income;
-    private List<String>list_income;
-    private ArrayAdapter<String>adapter_income;
-    //spinner career
-    private Spinner spinner_career;
-    private List<String>list_career;
-    private ArrayAdapter<String>adapter_career;
-    //spinner marriage
-    private Spinner spinner_marriage;
-    private List<String>list_marriage;
-    private ArrayAdapter<String>adapter_marriage;
+    private Spinner spinner_area;
+    private List<String> list_area;
+    private ArrayAdapter<String> adapter_area;
     //Button Search
     private Button button_search;
-    //String url
-    private String gender_choice;
-    private String income_choice;
-    private String career_choice;
-    private String marriage_choice;
+    private String area_choice;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.population_info);
+        setContentView(R.layout.house_info);
 
         //spinner gender
-        spinner_gender = (Spinner) findViewById(R.id.spinner);
-        list_gender = new ArrayList<String>();
+        spinner_area = (Spinner) findViewById(R.id.spinner);
+        list_area = new ArrayList<String>();
         //TODO:从服务器端获取数据库信息，并自动生成list来替换以下代码
-        list_gender.add("性别");
-        list_gender.add("男");
-        list_gender.add("女");
-        adapter_gender = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list_gender);
-        adapter_gender.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner_gender.setAdapter(adapter_gender);
-        spinner_gender.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        list_area.add("区域");
+        list_area.add("A1");
+        list_area.add("B1");
+        adapter_area = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list_area);
+        adapter_area.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_area.setAdapter(adapter_area);
+        spinner_area.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 if (i != 0) {
-                    gender_choice = adapter_gender.getItem(i);
+                    area_choice = adapter_area.getItem(i);
                 } else {
-                    gender_choice = "";
+                    area_choice = "";
                 }
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
-                gender_choice = "";
+                area_choice = "";
             }
         });
 
@@ -79,9 +63,11 @@ public class HouseAty extends Activity {
         button_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO:上传搜索条件，并获取搜索结果
-                String url = "202.120.40.111:8080/community-server/information/search/" + "?" + gender_choice + "&" + income_choice + "&" + career_choice + "&" + marriage_choice;
-                startActivity(new Intent(getApplicationContext(), SearchResult.class));
+                Intent intent=new Intent();
+                intent.putExtra("mode","House");
+                intent.putExtra("gender", area_choice);
+                intent.setClass(HouseAty.this, SearchResult.class);
+                startActivity(intent);
             }
         });
     }
--- SmartCity/src/sjtu/dclab/smartcity/ui/info_look_up/SearchResult.java
+++ SmartCity/src/sjtu/dclab/smartcity/ui/info_look_up/SearchResult.java
@@ -1,6 +1,5 @@
 package sjtu.dclab.smartcity.ui.info_look_up;
 
-import com.google.gson.Gson;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,11 +7,16 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import sjtu.dclab.smartcity.R;
+import sjtu.dclab.smartcity.model.Apartment;
+import sjtu.dclab.smartcity.model.CitizenResident;
+import sjtu.dclab.smartcity.tools.GsonTool;
 import sjtu.dclab.smartcity.webservice.BasicWebService;
-import sjtu.dclab.smartcity.ui.info_look_up.Population;
 
 /**
  * Created by theGODofws on 2015/8/12.
@@ -20,18 +24,18 @@ import sjtu.dclab.smartcity.ui.info_look_up.Population;
 public class SearchResult extends Activity{
     //Gson
     private BasicWebService bws;
-    private Gson Gson;
+    private GsonTool Gson;
     private String url;
     //ListView
     private ListView listView;
     private ArrayAdapter<String>adapter;
-    private String[]result={};
+    private ArrayList<String> result;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_result);
-
+        result = new ArrayList<String>();
         url = "202.120.40.111:8080/community-server/rest/";
 
         //intent
@@ -41,22 +45,46 @@ public class SearchResult extends Activity{
         String condition2;
         String condition3;
         String condition4;
+
+        //Gson
+        Gson = new GsonTool();
+        bws = new BasicWebService();
         if (mode == "Population"){
             url = url+"citizen";
             condition1 = intent.getStringExtra("gender");
             condition2 = intent.getStringExtra("income");
             condition3 = intent.getStringExtra("career");
             condition4 = intent.getStringExtra("marriage");
+            List<CitizenResident>persons = Gson.getObjectList(bws.sendGetRequest(url,new HashMap<String, String>()),CitizenResident.class);
+            for(CitizenResident p : persons){
+                if ((condition1 == "" || p.getGender() == condition1)
+                        &&(condition4 == "" || p.getMarriage_status() == condition4)
+                        &&(condition3 == "" || p.getEmployment_status() == condition3)
+                        &&(condition2 == "" || p.getIncome_status() == condition2)){
+                    //TODO:加入列表，传递至下个页面，提供详细信息
+                    result.add(p.getName());
+                }
+            }
         }
         else {
             url = url+"apartment";
+            condition1 = intent.getStringExtra("area");
+            List<Apartment>apartments = Gson.getObjectList(bws.sendGetRequest(url,new HashMap<String, String>()),Apartment.class);
+            //TODO:确认房屋信息划分区域的标准，并修改此处判定的条件
+            for (Apartment a :apartments){
+                if (condition1 == ""){
+                    result.add(a.getName());
+                }
+                else if (condition1 == "A1"){
+                    if (Integer.parseInt(a.getArea()) < 80){
+                        result.add(a.getName());
+                    }
+                }
+                else if (Integer.parseInt(a.getArea()) >= 80){
+                    result.add(a.getName());
+                }
+            }
         }
-
-        //Gson
-        Gson = new Gson();
-        bws = new BasicWebService();
-        Population person = Gson.fromJson(bws.sendGetRequest(url,new HashMap<String, String>()),Population.class);
-
 
         listView = (ListView) findViewById(R.id.search_result);
         adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,result);
@@ -64,7 +92,7 @@ public class SearchResult extends Activity{
         listView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                //TODO:待添加详细信息页面，点击条目可查看详细信息
             }
         });
     }
